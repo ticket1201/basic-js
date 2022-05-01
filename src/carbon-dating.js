03553d4bcb66358ce3c91e71c03e70ac5e46788b
Anton Metlitsky <ticket1201@gmail.com>
2022-05-01 21:06:27 +0300

    feat: implement carbon dating func

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+
+
+  //find K
+  let k = 0.693/HALF_LIFE_PERIOD;
+
+
+  if(typeof(sampleActivity) === 'string' && parseInt(sampleActivity)){
+    if(sampleActivity <= MODERN_ACTIVITY && sampleActivity >= 0){
+      return Math.ceil( Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)) / k )
+    }
+    else return false
+  }
+  else return false
 }
 
 module.exports = {
